6a4c0ecf1b2e34a6e125a9fa83203a9767296b0c
longzhen <koreabnl@gmail.com>
2024-11-03 00:03:11 +0900

    update kotlin version to 1.9.24, add dependencies for compose

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/TopActivity.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2021 YUMEMI Inc. All rights reserved.
+ */
+package jp.co.yumemi.android.code_check
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import jp.co.yumemi.android.code_check.ui.theme.AppTheme
+import java.util.*
+
+class TopActivity : ComponentActivity() {
+
+    companion object {
+        lateinit var lastSearchDate: Date
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+            }
+        }
+    }
+
+}
+
+@Composable
+fun MyCompose() {
+    Text("hello compose")
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun TestPreview() {
+    AppTheme {
+        MyCompose()
+    }
+}
